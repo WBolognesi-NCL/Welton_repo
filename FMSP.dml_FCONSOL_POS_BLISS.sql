20b8a3e8d945adcf30ec5a30bef821a16042d64e
WBolognesi-NCL <146130824+WBolognesi-NCL@users.noreply.github.com>
2024-02-13 14:57:42 -0500

    Create FMSP.dml_FCONSOL_POS_BLISS.sql

---
+++ FMSP.dml_FCONSOL_POS_BLISS.sql
@@ -0,0 +1,19 @@
+ /* 
+=====================================================================
+    Date           : 10.06.2023
+    Author         : Welton Bolognesi
+    Request        : CHG0081975
+    Description    : Remove data from POS - Ship Bliss after 09/21/23
+	Database	   : FMSP
+	Schema		   : fconsol
+=====================================================================
+*/
+
+
+DELETE FROM fconsol.pos fp
+WHERE fp.pos_shp_id = 69
+AND fp.POS_SDAT >= to_date('2023-09-21','YYYY-MM-DD');
+
+-- 33.474 Rows to be deleted
+
+-- COMMIT;
